5d6e3552a7e6f8a7f3225efbdffe851d3b966a4d
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-10-07 22:36:35 +0300

    Update alive.js

--- scs/alive.js
+++ scs/alive.js
@@ -1,5 +1,5 @@
 const { zokou } = require('../framework/zokou');
-const { addOrUpdateDataInAlive, getDataFromAlive } = require('../lib/alive');
+const { addOrUpdateDataInAlive, getDataFromAlive } = require('../bdd/alive');
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
 const path = require("path");
